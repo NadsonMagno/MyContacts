44cae2caa9261257f13efefe71257c801be42cb7
NadsonMagno <143035086+NadsonMagno@users.noreply.github.com>
2026-02-06 21:48:25 +0000

    refactor: refatorando a logica de HomePage

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,55 @@
+import { Container, InputSearchContainer, Header,  ListContainer, Card } from "./style";
 
-
+/* import arrow from '../../assets/images/icons/arrow.svg';
+ */
 export default function Home() {
-    return (
-        <h1 >Home Page</h1>
-    )
-    }
+  return (
+  <Container>
+    {/*             <img src={logo} alt="Logo" />
+ */}
+            <InputSearchContainer>
+                <input type="text" placeholder="Pesquisar contato..." />
+            </InputSearchContainer>
+    <Header>
+      <strong>3 contatos</strong>
+      <a href="/">Adicionar novo contato</a>
+    </Header>
+
+    <ListContainer>
+        <header>
+            <button type="button" className="sor-button">
+                <span>Nome</span>
+{/*                 <img src={arrow} alt="Arrow icon" />
+ */}            </button>
+        </header>
+
+
+    </ListContainer>
+
+    <Card>
+        <div className="info">
+            <div className="contact-name">
+                <strong>Bruno Silva</strong>
+                <small>instagram</small>
+            </div>
+        <span>buno@gmail.com</span>
+        <span>(11) 99999-9999</span>
+
+        </div>
+
+        <div className="actions">
+            <a href="/">
+{/*                 <img src={edit} alt="Edit" />
+ */}        </a>
+            <button type="button">
+{/*                 <img src={trash} alt="Trash" />
+ */}        </button>
+
+        </div>
+
+    </Card>
+
+
+  </Container>
+);
+}
